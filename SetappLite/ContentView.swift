03e6b55c001e7c94f9214a96284e36ebe9459bee
Emanuel Mairoll <accounts@emanuelmairoll.at>
2026-02-25 18:00:53 +0100

    Initial Commit

---
+++ SetappLite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SetappLite
+//
+//  Created by Emanuel Mairoll on 25.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SetappLite/SetappLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetappLiteApp.swift
+//  SetappLite
+//
+//  Created by Emanuel Mairoll on 25.02.26.
+//
+
+import SwiftUI
+
+@main
+struct SetappLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
